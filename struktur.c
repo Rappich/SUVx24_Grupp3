2a5423f5ee03bb8a66652e151fc8180717fdb3ea
Rappisch <fredrik.martensson@chasacademy.se>
2024-09-23 15:24:01 +0200

    Update struktur.c

--- struktur.c
+++ struktur.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 
 //Structure for grouproom variables
 struct groupRoom
 {
     int roomNumber; 
-    bool availability;
     int roomSize;
     int time[10];
 };
@@ -16,15 +14,34 @@ int main ()
 
 {
 int i;
+int j;
 
     struct groupRoom room[5];
 
     for(i = 0;i<5;i++)
     {
-        room[i].availability = true;
         room[i].roomSize = 8;
-        room[i].time
+        room[i].roomNumber = i+1;
+
+        for (j = 0; j<10;j++)
+        {
+            room[i].time[j] = 1;
+        }
+        
     }
+
+Vilken tid?
+"0: 8:00"
+"1: 9:00"
+
+"5:  13.00"
+användaren skriver 5 (= 13.00)
+for (loopa igenom varje rum och kolla ledighet vid 13.00)
+
+printa ut alla lediga tider
+if(room[i].time[5] == 1)
+
+print: room[i].roomNumber är ledigt klockan %d 13:00
 
     return 0;
 }
